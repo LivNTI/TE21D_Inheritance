1df21b8c9c3fb56d0199704e4464131a74d41c59
LivNTI <107177980+LivNTI@users.noreply.github.com>
2023-11-09 10:43:24 +0100

    INtital structure

--- src/Student.java
+++ src/Student.java
@@ -1,2 +1,23 @@
-package PACKAGE_NAME;public class Student {
+public class Student {
+    /** Attribute*/
+    String name;
+    int persNo;
+    String className;
+
+    /** Constructor */
+    public Student(String name, int persNo, String className){
+        this.name= name;
+        this.persNo = persNo;
+        this.className = className;
+    }
+
+    /** Methods */
+    public void changeName(String name){
+        this.name= name;
+    }
+
+    public void changeClass(String className){
+        this.className= className;
+    }
 }
+
--- src/Teacher.java
+++ src/Teacher.java
@@ -1,2 +1,24 @@
-package PACKAGE_NAME;public class Teacher {
+import java.util.ArrayList;
+
+public class Teacher {
+    /** Attribute */
+    String name;
+    int persNo;
+    ArrayList<String> courses= new ArrayList<>();
+
+    /** Constructor */
+    public Teacher(String name, int persNo){
+        this.name= name;
+        this.persNo = persNo;
+    }
+
+    /** Methods */
+    public void changeName(String name){
+        this.name= name;
+    }
+
+    public void addCourse(String course){
+        courses.add(course);
+    }
 }
+
